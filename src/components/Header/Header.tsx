19711fd7f59fd69ed8fe0e51e0ca26e3877f3c06
Oleksandr <oleksandrshtonda@gmail.com>
2024-03-25 15:29:26 +0100

    create 2 empty components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react';
+import './Header.scss';
+
+interface Props {}
+
+export const Header: FC<Props> = () => {
+  return (
+    <>
+      <header className="header">
+      
+      </header>
+      
+      <hr className="header-hr"/>
+    </>
+  );
+};
